4f1f3a2182185dbac252323e2e81662931c17237
serg <nautbek@gmail.com>
2024-01-23 17:53:19 +0300

    Add shops tab.

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/shops', function () {
+        return view('shops');
+    })->name('shops');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
