791fa26f6b6b71ff15a890672c70e135060063db
serg <nautbek@gmail.com>
2024-01-27 09:20:54 +0300

    Add translations for login page.

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -2,4 +2,7 @@
 return [
     'log_in' => 'Log in',
     'forgot_your_password' => 'Forgot your password?',
+    'email' => 'Email',
+    'password' => 'Password',
+    'remember_me' => 'Remember me',
 ];
--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -2,4 +2,7 @@
 return [
     'log_in' => 'Войти',
     'forgot_your_password' => 'Забыли пароль?',
+    'email' => 'Email',
+    'password' => 'Пароль',
+    'remember_me' => 'Запомнить меня',
 ];
