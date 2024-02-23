ab507e0a4190a6c8205db587f6cf2914bd52f14f
serg <nautbek@gmail.com>
2024-02-23 14:21:55 +0300

    Fix setButton body (3).

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -76,7 +76,7 @@ class TelegramService
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($buttonData));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $buttonData);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
