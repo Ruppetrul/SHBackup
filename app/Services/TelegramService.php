1c82c14a3405c6684256b6136aeede3055b56ba6
serg <nautbek@gmail.com>
2024-02-23 14:17:02 +0300

    Fix setButton body (2).

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -64,13 +64,13 @@ class TelegramService
         $url = 'https://api.telegram.org/bot'. $this->token. '/setChatMenuButton';
 
         $buttonData = [
-            'menu_button' => [
+            'menu_button' => json_encode([
                 'type' => 'web_app',
                 'text' => 'Каталог',
                 'web_app' => [
                     'url' => url('/') . '/mini/' . $shopName
                 ],
-            ]
+            ])
         ];
 
         $ch = curl_init();
