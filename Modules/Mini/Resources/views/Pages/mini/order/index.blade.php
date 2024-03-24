120dc06b39d034cb96445e997518ad659a823fc8
serg <nautbek@gmail.com>
2024-03-24 13:31:47 +0300

    Fix getCartData function using 3.

--- Modules/Mini/Resources/views/Pages/mini/order/index.blade.php
+++ Modules/Mini/Resources/views/Pages/mini/order/index.blade.php
@@ -4,7 +4,7 @@
 <section class="section-b-space shop-section">
     <div class="container-fluid-lg">
         <div class="row">
-            @include('Mini::Pages.mini.section.order', ['products' => $miniRepo->getLatestActiveProducts()])
+            @include('Mini::Pages.mini.section.order', ['products' => ShopEloquent::getLatestActiveProducts()])
         </div>
     </div>
 </section>
