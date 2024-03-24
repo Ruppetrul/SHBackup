fc3e929081fb99da2cda64c7c4cea4cc56e1c8b5
serg <nautbek@gmail.com>
2024-03-24 13:14:18 +0300

    Test 1.

---
+++ Modules/Mini/Http/Controllers/YookassaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Mini\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Log;
+
+class YookassaController extends Controller
+{
+    public function test(Request $request) {
+        Log::debug('New request from Yookassa');
+        Log::debug($request);
+    }
+}
