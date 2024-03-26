c29bae9ce1ed6f888b5c990d4266010dae0f6497
serg <nautbek@gmail.com>
2024-03-26 20:58:57 +0300

    Add only_data param for 'products' endpoint.

--- Modules/Mini/Http/Controllers/MiniController.php
+++ Modules/Mini/Http/Controllers/MiniController.php
@@ -79,18 +79,21 @@ class MiniController extends Controller
         try {
             $activeProducts = $miniRepo->getActive($request);
 
+            if ($request->get('only_data')) {
+                $result['view'] = view(
+                    'Mini::Pages.mini.section.products',
+                    [
+                        'products'    => $activeProducts,
+                        'cart_detail' => $cart_detail,
+                        'shopId'      => app('current_shop_id'),
+                        'shopName'    => app('current_shop_name'),
+                    ]
+                )->render();
+            } else {
+                $result['products'] = $activeProducts;
+            }
             $result['total'] = count($activeProducts);
             $result['has_more'] = ($result['total'] >= 10);
-
-            $result['view'] = view(
-                'Mini::Pages.mini.section.products',
-                [
-                    'products'    => $activeProducts,
-                    'cart_detail' => $cart_detail,
-                    'shopId'      => app('current_shop_id'),
-                    'shopName'    => app('current_shop_name'),
-                ]
-            )->render();
         } catch (Exception $exception) {
             $result['success'] = false;
         }
