c453bd50a069934fa17cd8099951fb2bc31384a2
serg <nautbek@gmail.com>
2024-04-25 21:27:47 +0300

    Docker prepare.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ while ! nc -z mysql 3306; do
     sleep 1
 done
 
-#php artisan migrate --force
-#php artisan db:seed --class=SearchSeeder
+php artisan migrate --force
+php artisan db:seed --class=DevSeeder
 
 exec apache2-foreground
