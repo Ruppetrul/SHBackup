7529ce0874d3918a4cb479489d3e161d58a37470
serg <nautbek@gmail.com>
2024-05-07 20:20:07 +0300

    Remove deprecated js files.

--- Modules/Mini/Resources/views/section/js.blade.php
+++ Modules/Mini/Resources/views/section/js.blade.php
@@ -1,31 +1,5 @@
-{{-- latest jquery --}}
-<script src="{{ asset('home/js/jquery-3.6.0.min.js') }}"></script>
-
-{{-- jquery ui --}}
-<script src="{{ asset('home/js/jquery-ui.min.js') }}"></script>
-
 {{-- Bootstrap js --}}
 <script src="{{ asset('home/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('home/js/bootstrap/bootstrap-notify.min.js') }}"></script>
 
-<script src="{{ asset('home/js/bootstrap/popper.min.js') }}"></script>
-
-{{-- feather icon js --}}
-<script src="{{ asset('home/js/feather/feather.min.js') }}"></script>
-<script src="{{ asset('home/js/feather/feather-icon.js') }}"></script>
-
-{{-- Lazyload Js --}}
-<script src="{{ asset('home/js/lazysizes.min.js') }}"></script>
-
-{{-- Slick js --}}
-<script src="{{ asset('home/js/slick/slick.js') }}"></script>
-
-{{-- Auto Height Js --}}
-<script src="{{ asset('home/js/auto-height.js') }}"></script>
-
-{{-- Fly Cart Js --}}
-<script src="{{ asset('home/js/fly-cart.js') }}"></script>
-
-{{-- Quantity js --}}
-<script src="{{ asset('home/js/quantity-2.js') }}"></script>
 @yield('js')
