60acfacc5b1529b83ceea26be55e04526de94443
serg <nautbek@gmail.com>
2024-06-01 18:51:15 +0300

    Add main preloader.

---
+++ Modules/Mini/Resources/components/main/PreloaderItem.vue
@@ -0,0 +1,54 @@
+<script setup>
+</script>
+
+<template>
+  <div class="item_cart_container">
+    <div class="item_cart">
+      <div class="preloader_image col-2">
+      </div>
+      <div class="preloader_name">
+      </div>
+      <div class="preloader_price">
+      </div>
+      <div class="preloader_control_panel">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .item_cart_container {
+    padding: 5px;
+  }
+
+  .preloader_image, .preloader_name, .preloader_price , .preloader_control_panel {
+      background-color: #f0f0f0;
+      height: 1em;
+      animation: pulse 0.5s infinite alternate;
+      border-radius: 5px;
+  }
+
+  .preloader_image {
+      width: 100%;
+      height: 15em;
+      min-width: 4em;
+  }
+
+  .preloader_name {
+      width: 50%;
+      min-height: 1em;
+      margin-top: 1em;
+  }
+
+  .preloader_price {
+      width: 25%;
+      min-height: 1em;
+      margin-top: 1em;
+  }
+
+  .preloader_control_panel {
+      width: 100%;
+      min-height: 2em;
+      margin-top: 1em;
+  }
+</style>
---
+++ Modules/Mini/Resources/components/main/PreloaderItemsPanel.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import PreloaderItem from './PreloaderItem.vue';
+</script>
+
+<template>
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5" id="items_panel">
+    <PreloaderItem v-for="index in 10" :key="index"/>
+  </div>
+</template>
+
+<style scoped>
+  #items_panel {
+    padding: 10px;
+  }
+</style>
