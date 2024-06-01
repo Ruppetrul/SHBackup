1491c576a655474a55917fe355ab9901e230dfda
serg <nautbek@gmail.com>
2024-06-01 17:11:09 +0300

    Add count to cart.

--- Modules/Mini/Resources/components/cart/Item.vue
+++ Modules/Mini/Resources/components/cart/Item.vue
@@ -9,8 +9,18 @@ const props = defineProps(['item'])
         <div class="item_name">
             {{ props.item.title }}
         </div>
+        <div class="item_count">
+            {{ props.item.count }}
+        </div>
     </div>
 </template>
 
 <style scoped>
+.cart_item {
+    display: flex;
+    justify-content: space-between;
+}
+.item_count {
+    text-align: right;
+}
 </style>
