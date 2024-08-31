bb133b09622531cb9aebd3146e146a4247f0c8b7
nautbek <nautbek@gmail.com>
2024-08-31 10:29:07 +0300

    Fix null background-color.

--- Modules/Mini/Resources/views/layouts/mini.blade.php
+++ Modules/Mini/Resources/views/layouts/mini.blade.php
@@ -6,7 +6,7 @@
         <script src="https://telegram.org/js/telegram-web-app.js"></script>
         @include('Mini::section.css') {{-- Include css files --}}
     </head>
-    <body class="bg-effect" style="background-color: {{$page['props']['company_data']['background-color']}}">
+    <body class="bg-effect" style="background-color: {{$page['props']['company_data']['background-color'] ?? null}}">
         @inertia
         @yield('content') {{-- Yield content data --}}
     </body>
