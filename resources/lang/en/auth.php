6a5b7676e97dc3a368720b33b31ed8f4727cd2ba
nautbek <nautbek@gmail.com>
2024-10-05 19:15:13 +0300

    Add bad credentials translate.

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -17,4 +17,5 @@ return [
     'thanks_message' => 'Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.',
     'verification_link_sent' => 'A new verification link has been sent to the email address you provided during registration.',
     'resend_verification_email' => 'Resend Verification Email',
+    'failed' => 'Incorrect email or password',
 ];
